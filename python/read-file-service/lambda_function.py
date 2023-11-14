e6740bc2c1197dc6a00ee5e3a9dc4568669f025d
CardonaDeveloperZabud <julio.cardona@zabud.com.co>
2023-11-13 20:03:13 -0500

    state machine

--- python/read-file-service/lambda_function.py
+++ python/read-file-service/lambda_function.py
@@ -11,10 +11,11 @@ def lambda_handler(event, context):
 
     # Crear cliente de S3
     s3_client = boto3.client('s3')
-    print("contenido del evento: " , json.dumps(event))
     # Descargar el archivo desde S3
     response = s3_client.get_object(Bucket=bucket_name, Key=file_key)
     file_content = response['Body'].read()
+    metadata = response['Metadata']
+    client_email = metadata.get('email')
 
     # Verificar el tipo de archivo basado en la extensión
     file_extension = file_key.split('.')[-1].lower()
@@ -34,11 +35,13 @@ def lambda_handler(event, context):
     # Convertir el DataFrame a formato JSON
     json_data = df.to_json(orient='records')
     # Imprimir el resultado (puedes hacer otras operaciones con los datos)
-    print("JSONDATA",json_data)
+    print("JSON_DATA: ",json_data)
 
     # Aquí puedes realizar otras operaciones o almacenar los datos en otro lugar
 
     return {
         'statusCode': 200,
-        'body': 'Procesamiento completado.'
+        'body': 'Procesamiento completado.',
+        'client_email' : client_email,
+        'json_data' : json_data
     }
